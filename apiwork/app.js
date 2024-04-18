098f430aedbd393544736b4f22867ca8bc50f154
kristifristi <Kristiaancramer@gmail.com>
2024-04-18 14:51:07 +0200

    api now expects the cool new standard where someones pin isn't in the url
    it's plain text in the body instead

--- apiwork/app.js
+++ apiwork/app.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-const sql = require("./queries");
+const sql = require("./sqlqueries");
 
 const noobtoken = require("./envparse").noobtoken;
 const ourIban = require("./envparse").ourIban;
 
 const SHA256 = require("crypto-js/sha256");
-function sha256Stringify(uid, pin) {
-    return(SHA256(uid + pin).toString());
+function sha256Stringify(uid, pincode) {
+    return(SHA256(uid + pincode).toString());
 }
 
 
@@ -15,19 +15,24 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+//cargo cult programming
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.raw());
 
-const bodyParser = require("body-parser");
-const rawparser = bodyParser.raw();
-const textparser = bodyParser.text();
-//welcome to callback hell!
-
-
+//imagine actually validating iban
 function validateIban(iban){
     const valid = new RegExp("[A-Z]{2}\\d{2}[A-Z]{4}\\d{10}");
     return valid.test(iban);
+}
+
+function tokenCheck(headers){
+    if(headers['noob-token']){
+        if(headers['noob-token'] == noobtoken){
+            return true;
+        }
+    }
+    return false;
 }
 
 app.get('/api/noob/health', (req, res) => {
@@ -35,24 +40,27 @@ app.get('/api/noob/health', (req, res) => {
 });
 
 function infoApi (req,res) {   
-    let queries = req.query
-    if(!queries.iban){
+    let body = req.body
+    if(!tokenCheck(req.headers)){
+        res.status(401).send("request not authorised with noob-token");
+    }
+    if(!body.target){
         res.status(400).send("missing iban");
         return;
     }
-    if(!queries.pin){
-        res.status(400).send("missing pin");
+    if(!body.pincode){
+        res.status(400).send("missing pincode");
         return;
     }
-    if(!queries.uid){
+    if(!body.uid){
         res.status(400).send("missing uid");
         return;
     }
 
-    if(sql.hasInjection(queries.iban) || sql.hasInjection(queries.pin) || sql.hasInjection(queries.uid)){
+    if(sql.hasInjection(body.uid)){
         console.log("injection attempt! attacker info: ");
         console.log(req.ip);
-        console.log(queries);
+        console.log(body);
         console.log(req.headers)
         res.status(418).send("you think you're smart don't you?");
         return;
@@ -66,7 +74,7 @@ function infoApi (req,res) {
     from Card
     join Account on Card.account = idAccount
     join People on Account.owner = idPeople
-    where uid =  "${queries.uid}"`, (results) => {
+    where uid =  "${body.uid}"`, (results) => {
         if(results.error){
             res.status(500).send("database error");
             return;
@@ -78,7 +86,7 @@ function infoApi (req,res) {
             return;
         }
 
-        realIban = queries.iban.toUpperCase();
+        realIban = body.target.toUpperCase();
         if(realIban != result.iban){
             res.status(404).send("iban mismatch");
             return;
@@ -90,7 +98,7 @@ function infoApi (req,res) {
             res.status(403).send("card blocked after too many attempts");
         }
 
-        let genHash = sha256Stringify(queries.uid,queries.pin.toString());
+        let genHash = sha256Stringify(body.uid,body.pincode.toString());
         if(genHash != result.hash){
             res.status(401).json({"attempts_remaining" : result.chances});
             sql.dbquery(sql.realpool, `update Card set wrongAttemptsDone = wrongAttemptsDone + 1 where idCard = "${result.idCard}"`, (results) => {});
@@ -98,27 +106,29 @@ function infoApi (req,res) {
         }
 
         res.status(200).json({ 
-            'firstname' : result.fName,
-            'lastname' : result.lName,
+        'firstname' : result.fName,
+        'lastname' : result.lName,
         'balance' : result.balance,
         'accountname' : result.aName});
     });
 };
 
-app.get('/api/noob/accountinfo', infoApi);
-app.get('/api/accountinfo', infoApi);
+app.post('/api/accountinfo', infoApi);
 
 function withdrawApi (req,res) {
-    let queries = req.query
-    if(!queries.iban){
+    let body = req.body
+    if(!tokenCheck(req.headers)){
+        res.status(401).send("request not authorised with noob-token");
+    }
+    if(!body.target){
         res.status(400).send("missing iban");
         return;
     }
-    if(!queries.pin){
-        res.status(400).send("missing pin");
+    if(!body.pincode){
+        res.status(400).send("missing pincode");
         return;
     }
-    if(!queries.uid){
+    if(!body.uid){
         res.status(400).send("missing uid");
         return;
     }
@@ -126,10 +136,10 @@ function withdrawApi (req,res) {
         res.status(400).send("missing amount");
     }
 
-    if(sql.hasInjection(queries.iban) || sql.hasInjection(queries.pin) || sql.hasInjection(queries.uid)){
+    if(sql.hasInjection(body.uid)){
         console.log("injection attempt! attacker info: ");
         console.log(req.ip);
-        console.log(queries);
+        console.log(body);
         console.log(req.headers)
         res.status(418).send("you think you're smart don't you?");
         return;
@@ -141,7 +151,7 @@ function withdrawApi (req,res) {
         Account.maxNegativeBalance as maxRed, Account.idAccount as idAccount
     from Card
     join Account on Card.account = idAccount
-    where uid = "${queries.uid}"`, (results) => {
+    where uid = "${body.uid}"`, (results) => {
         if(results.error){
             res.status(500).send("database error");
             return;
@@ -154,8 +164,8 @@ function withdrawApi (req,res) {
             return;
         }
 
-        queries.iban = queries.iban.toUpperCase();
-        if(result.iban != queries.iban){
+        let realIban = body.target.toUpperCase();
+        if(result.iban != realIban){
             res.status(404).send("iban mismatch");
             return;
         }
@@ -173,7 +183,7 @@ function withdrawApi (req,res) {
             res.status(412).send("account balance too low");
         }
 
-        let genHash = sha256Stringify(queries.uid,queries.pin);
+        let genHash = sha256Stringify(body.uid,body.pincode);
         if(genHash != result.hash){
             res.status(401).json({"attempts_remaining" : result.chances});
             sql.dbquery(sql.realpool, `update Card set wrongAttemptsDone = wrongAttemptsDone + 1 where idCard = ${result.idCard}`, (results) => {});
@@ -189,7 +199,6 @@ function withdrawApi (req,res) {
     });
 }
 
-app.post('/api/noob/withdraw', withdrawApi);
 app.post('/api/withdraw', withdrawApi);
 
 
