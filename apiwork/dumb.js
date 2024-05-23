bc1c67ba77ad6efce7fda919702014d5c9245a86
kristifristi <Kristiaancramer@gmail.com>
2024-05-23 16:48:59 +0200

    slightly more intelligent dummy

--- apiwork/dumb.js
+++ apiwork/dumb.js
@@ -8,9 +8,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.text());
 
-app.all("/api/*",(req, res)=> {
-    res.status(200).send("OK");
+app.all("/api/accountinfo",(req, res)=> {
+    res.status(200).send({
+        'firstname' : 'dummy',
+        'lastname' : 'dummy',
+        'balance' : -1
+    });
 });
+
+app.all("/api/*", (req, res) => {
+    res.send("OK");
+})
 
 app.listen(8420, () => {
     console.log("teehee!")
